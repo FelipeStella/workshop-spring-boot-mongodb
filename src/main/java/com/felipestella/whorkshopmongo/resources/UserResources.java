c8abf3fcb14def54d5f320f60f61a125e3671318
FelipeStella <felipedasilva.stella@gmail.com>
2020-02-29 14:11:44 -0300

    Classe de transferência de dados (DTO) do usuário.

--- src/main/java/com/felipestella/whorkshopmongo/resources/UserResources.java
+++ src/main/java/com/felipestella/whorkshopmongo/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.felipestella.whorkshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipestella.whorkshopmongo.domain.User;
+import com.felipestella.whorkshopmongo.dto.UserDto;
 import com.felipestella.whorkshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResources {
 	private UserService servico;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDto>> findAll(){
 		List<User> listUsuario = servico.findAll();
-		return ResponseEntity.ok().body(listUsuario);		
+		List<UserDto> listUsuarioDto = listUsuario.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listUsuarioDto);		
 	}
 }
