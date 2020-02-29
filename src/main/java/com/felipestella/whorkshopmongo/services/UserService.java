5ec907e9b820be020102ec0db0b000c8b87f5669
FelipeStella <felipedasilva.stella@gmail.com>
2020-02-29 15:08:32 -0300

    Obtendo um usuário por Id - Tratando exceções

--- src/main/java/com/felipestella/whorkshopmongo/services/UserService.java
+++ src/main/java/com/felipestella/whorkshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.felipestella.whorkshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipestella.whorkshopmongo.domain.User;
 import com.felipestella.whorkshopmongo.repository.UserRepository;
+import com.felipestella.whorkshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repositorio.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> usuario = repositorio.findById(id);
+		return usuario.orElseThrow(() -> new ObjectNotFoundException("Comando não encontrado"));
+		
 	}
 }
