6b8dca43290fcbd1350ddb61c69daa917918f436
FelipeStella <felipedasilva.stella@gmail.com>
2020-03-03 12:50:19 -0300

    Implementação dos métodos insert, delete e update

--- src/main/java/com/felipestella/whorkshopmongo/resources/UserResources.java
+++ src/main/java/com/felipestella/whorkshopmongo/resources/UserResources.java
@@ -1,14 +1,20 @@
 package com.felipestella.whorkshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipestella.whorkshopmongo.domain.User;
 import com.felipestella.whorkshopmongo.dto.UserDto;
@@ -19,18 +25,40 @@ import com.felipestella.whorkshopmongo.services.UserService;
 public class UserResources {
 	
 	@Autowired
-	private UserService servico;
+	private UserService service;
 	
 	@GetMapping
 	public ResponseEntity<List<UserDto>> findAll(){
-		List<User> listUsuario = servico.findAll();
-		List<UserDto> listUsuarioDto = listUsuario.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listUsuarioDto);		
+		List<User> listUser = service.findAll();
+		List<UserDto> listUserDto = listUser.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listUserDto);		
 	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDto> findById(@PathVariable String id){
-		User usuario = servico.findById(id);
-		return ResponseEntity.ok().body(new UserDto(usuario));
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDto(user));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDto objDto){
+		User newUser = service.fromDto(objDto);
+		newUser = service.insert(newUser);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDto> update(@RequestBody UserDto objDto, @PathVariable String id){
+		User updateDataUser = service.fromDto(objDto);
+		updateDataUser = service.update(id, updateDataUser);
+		return ResponseEntity.ok().body(new UserDto(updateDataUser));
+	}
+	
 }
--- src/main/java/com/felipestella/whorkshopmongo/services/UserService.java
+++ src/main/java/com/felipestella/whorkshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipestella.whorkshopmongo.domain.User;
+import com.felipestella.whorkshopmongo.dto.UserDto;
 import com.felipestella.whorkshopmongo.repository.UserRepository;
 import com.felipestella.whorkshopmongo.services.exception.ObjectNotFoundException;
 
@@ -14,15 +15,43 @@ import com.felipestella.whorkshopmongo.services.exception.ObjectNotFoundExceptio
 public class UserService {
 
 	@Autowired
-	private UserRepository repositorio;
+	private UserRepository reposit;
 	
 	public List<User> findAll(){
-		return repositorio.findAll();
+		return reposit.findAll();
 	}
 	
 	public User findById(String id) {
-		Optional<User> usuario = repositorio.findById(id);
+		Optional<User> usuario = reposit.findById(id);
 		return usuario.orElseThrow(() -> new ObjectNotFoundException("Comando não encontrado"));
 		
 	}
+	
+	public User insert(User obj) {
+		return reposit.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		reposit.deleteById(id);		
+	}
+	
+	public User update(String id, User newUser) {
+		User oldUser = findById(id);
+		oldUser = updateData(oldUser, newUser);
+		return reposit.save(oldUser);
+	}
+	
+	public User updateData(User oldUser, User newUser) {
+		oldUser.setName(newUser.getName());
+		oldUser.setEmail(newUser.getEmail());
+		return oldUser;
+	}
+	
+	public User fromDto(UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	
+	
 }
